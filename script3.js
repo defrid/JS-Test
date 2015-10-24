b6da693cc71c1c569e2cb7cdde5b45c6f852e335
mkalmykov <m.n.kalmykov@gmail.com>
2015-10-24 17:19:28 +0300

    Fixed Utils & Abstract Factory

--- script3.js
+++ script3.js
@@ -10,9 +10,43 @@ function Utils() {
 	
 	Utils.check = this;
 	
-	this.reduce = {};
-	this.deepEquals = {};
-	this.deepCopy = {};
+	this.reduce = function(arr, combine) {
+		var res = arr[0];
+		for(i = 1; i < arr.length; i ++) {
+			res = combine(res, arr[i]); 
+		};
+		return res;
+	};
+	
+	this.deepEquals = function(obj1, obj2) {
+		if (obj1 === obj2) {
+			return true;
+		}
+		if (typeof(obj1) != "object" || obj1 === null || typeof(obj2) != "object" || obj2 === null) {
+			return false;
+		}
+ 
+		for (var property in obj1) {
+			if (!(obj1.hasOwnProperty(property) && obj2.hasOwnProperty(property)) || !deepEqual(obj1[property], obj2[property])) {
+				return false;        
+			}
+		}
+		return true;
+	};
+	
+	this.deepCopy = function(obj) {
+		var newObj = {};
+			for(var key in obj) {
+				if(typeof obj[key] != "object" || obj[key] === null){
+					var property = obj[key];
+					newObj[key] = property;
+				}
+				else {
+					newObj[key] = deepCopy(obj[key]);
+				}
+			}
+		return newObj;
+	};
 }
 
 //Utils через замыкание
@@ -24,7 +58,7 @@ var closure = function() {
 		if(instance) {
 			return instance;
 		}
-		else return function newUtils() {
+		else return function() {
 			this.reduce = {};
 			this.deepEquals = {};
 			this.deepCopy = {};
@@ -39,29 +73,74 @@ console.log(obj1 === obj2);
 
 //Abstract Factory
 
-function abstractClass() {
-	
+function extend(Child, Parent) {
+	var F = function() {};
+	F.prototype = Parent.prototype;
+	Child.prototype = new F();
+	Child.prototype.constructor = Child;
+	Child.superclass = Parent.prototype;
 }
 
-function inheritor1() {
+//Создаем абстрактный класс "танк"
+function tank() {
+	this.hasTracks = true;
+	this.color = "green";
 	
-}
-inheritor1.prototype = new abstractClass();
-
-function inheritor2() {
-	
-}
-inheritor2.prototype = new abstractClass();
-
-function abstractFactory() {
-	
+	//Расстояние, которое танк может проехать
+	this.distance = function(fuel) {};
 }
 
-concrteFactory1 = function() {
-	return new inheritor1();
+//Реализация абстрактного класса (его наследники)
+
+//Легкий танк
+function lightTank() {
+	this.distance = function(fuel) {
+		return fuel * 5;
+	}
+}
+
+extend(lightTank, tank);
+
+
+
+//Тяжелый танк
+function heavyTank() {
+	this.distance = function(fuel) {
+		return fuel * 3;
+	}
+}
+
+extend(heavyTank, tank);
+
+
+//Абстрактная фабрика
+
+function tankFactory() {
+	this.createTank = function() {
+		return new tank;
+	};
+}
+
+//Фабрика легких танков
+lightTankFactory = function() {
+	this.createTank = function() {
+		return new lightTank();
+	}
 };
-concreteFactory2 = function() {
-	return new inheritor2();
+
+extend(lightTankFactory, tankFactory);
+
+
+//Фабрика тяжелых танков
+heavyTankFactory = function() {
+	this.createTank = function() {
+		return new heavyTank();
+	}
 }
 
-console.log()
+extend(heavyTankFactory, tankFactory);
+
+var inst = new lightTankFactory;
+var test = inst.createTank();
+
+console.log(test instanceof tank);

de65298050f6414ae26306a205d97594ef9afa4c
mkalmykov <m.n.kalmykov@gmail.com>
2015-11-23 19:48:36 +0300

    controllerTests Test #1

--- framework/directives/sg-controller.js
+++ framework/directives/sg-controller.js
@@ -1,46 +1,56 @@
+Provider.directive("sg-controller", function() {
+    return {
+        hasScope: true,
+        link: function(scope, element, expr) {
+            var controller = Provider.$get(expr + Provider.CONTROLLER_POSTFIX);
+            controller(scope);
+        }
+    };
+});
+
 var controllerTests = {
-	Test1: function () {
-		var controller = Provider.$get("sg-controller" + Provider.DIRECTIVE_POSTFIX);
+    Test1: function () {
+        var controller = Provider.$get("sg-controller" + Provider.DIRECTIVE_POSTFIX);
 
-		Provider.Controller("Ctrl", function ($scope) {
-			$scope.value = true;
-		});
+        Provider.Controller("Ctrl1", function ($scope) {
+            $scope.value = true;
+        });
 
-		console.assert(controller);
-		console.assert(controller.hasScope);
+        console.assert(controller);
+        console.assert(controller.hasScope);
 
-		var $rootScope = Provider.$get('$rootScope');
+        var $rootScope = Provider.$get('$rootScope');
 
-		var scope = $rootScope.$new();
+        var scope = $rootScope.$new();
 
-		controller.link(scope, null, "Ctrl");
+        controller.link(scope, null, "Ctrl1");
 
-		console.assert(scope.value);
-		console.log("Test1 success")
-	},
+        console.assert(scope.value);
+        console.log("Test1 success")
+    },
 
-	Test2: function() {
-		var controller = Provider.$get("sg-controller" + Provider.DIRECTIVE_POSTFIX);
+    Test2: function() {
+        var controller = Provider.$get("sg-controller" + Provider.DIRECTIVE_POSTFIX);
 
-		Provider.Controller("Ctrl2", function ($scope, Factory) {
-			$scope.value = true;
-		});
+        Provider.Controller("Ctrl2", function ($scope, Factory) {
+            $scope.value = true;
+        });
 
-		console.assert(controller);
-		console.assert(controller.hasScope);
+        console.assert(controller);
+        console.assert(controller.hasScope);
 
-		Provider.$register("Factory", function ($rootScope) {
-			console.log("Factory called");
-			return function () { };
-		});
+        Provider.$register("Factory", function ($rootScope) {
+            console.log("Factory called");
+            return function () { };
+        });
 
-		var $rootScope = Provider.$get('$rootScope');
+        var $rootScope = Provider.$get('$rootScope');
 
-		var scope = $rootScope.$new();
+        var scope = $rootScope.$new();
 
-		controller.link(scope, null, "Ctrl2");
+        controller.link(scope, null, "Ctrl2");
 
-		console.assert(scope.value);
-		console.log("Test2 success")
-	}
+        console.assert(scope.value);
+        console.log("Test2 success")
+    }
 };

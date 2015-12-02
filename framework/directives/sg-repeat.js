44ee0ba43b43e2be88018ecf7378e123b94596ac
mkalmykov <m.n.kalmykov@gmail.com>
2015-12-02 12:10:19 +0300

    Fix sg-repeat

--- framework/directives/sg-repeat.js
+++ framework/directives/sg-repeat.js
@@ -6,33 +6,33 @@ Provider.directive("sg-repeat", function() {
             var itemName = expr.match(/(\w+)\s+in\s+(\w+)/)[1]
             var arrName = expr.match(/(\w+)\s+in\s+(\w+)/)[2];
             var parentElement = element.parentElement;
+            var newElements = [element];
 
-            function repeat(scope, element, parentElement, itemName, arrName) {
+            function updateDOM(elem) {
                 for(var i = 0; i < scope[arrName].length; i++) {
                     var childScope = scope.$new();
                     childScope[itemName] = scope[arrName][i];
-                    var newElement = element.cloneNode(true);
+                    var newElement = elem.cloneNode(true);
+                    newElements.push(newElement);
                     parentElement.appendChild(newElement);
                     for(var j = 0; j < newElement.children.length; j++) {
                         Compiler.$compile(childScope, newElement.children[j]);
                     }
                 }
-                parentElement.removeChild(parentElement.children[0]);
+                parentElement.removeChild(elem);
+                newElements.shift();
             }
 
-            repeat(scope, element, parentElement, itemName, arrName);
+            updateDOM(newElements[0]);
 
-            scope.$watch(function() {
-                             return scope[arrName]; 
-                         },
-                         
-                         function() { 
-                             while(parentElement.children[1]) {
-                                 parentElement.removeChild(parentElement.children[1]);
+            scope.$watch(arrName,
+                         function() {
+                             for(var k = newElements.length - 1; k > 0; k--) {
+                                 parentElement.removeChild(newElements[k]);
+                                 newElements.pop();
                              }
-                             repeat(scope, element, parentElement, itemName, arrName);
-                         }
-            );
+                             updateDOM(newElements[0]);
+                         });
         }
     };
 });

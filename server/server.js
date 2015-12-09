b76558f220e89990b3f9bbfb14ad2b921a071750
mkalmykov <m.n.kalmykov@gmail.com>
2015-12-09 12:25:47 +0300

    Simple server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    request.on("data", function(chunk) {
+        response.write(chunk.toString().toUpperCase());
+    });
+    request.on("end", function() {
+        response.end();
+    });
+}).listen(8000);
+
+console.log("Server succesfully started");

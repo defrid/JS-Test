f233e6b5d934a25f133ceff26ca49b61eef1454c
mkalmykov <m.n.kalmykov@gmail.com>
2015-12-14 12:44:52 +0300

    Basic XMLHttpRequest

--- server/server.js
+++ server/server.js
@@ -1,12 +1,8 @@
 var http = require('http');
+
 http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    request.on("data", function(chunk) {
-        response.write(chunk.toString().toUpperCase());
-    });
-    request.on("end", function() {
-        response.end();
-    });
+    response.writeHead(200, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
+    response.end(JSON.stringify({value: 10}));
 }).listen(8000);
 
 console.log("Server succesfully started");

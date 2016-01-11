71789c2798b496b2645af6447f09573949978651
mkalmykov <m.n.kalmykov@gmail.com>
2016-01-11 22:59:24 +0300

    Add database

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
 
 var app = express();
 var port = 8000;
 
 app.use(bodyParser.json());
-
-function skip(from, to, source) {
-    var content = [];
-    if(to > source.length) {
-        to = source.length;
-    }
-    for(var i = from; i < to; i++) {
-        content.push(source[i]);
-    }
-    return content;
-}
-
-function getData(){
-    var data = JSON.parse(fs.readFileSync(__dirname + "/data.json", "utf-8"));
-    return data;
-}
 
 function generateId(currentData) {
     var id = Math.floor(1000000 + Math.random() * (1000000000 + 1 - 1000000)) + "a";
@@ -37,48 +22,57 @@ function generateId(currentData) {
     id = generateId();
 }
 
+function openDatabase() {
+    var db = new sqlite3.Database("data.db");
+    return db;
+}
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/build/index.html");
 });
 
 app.get('/page', function(req, res) {
-    var rawData = getData();
+    var from = (req.query.page - 1) * req.query.elementsOnPage;
+    var to = req.query.page * req.query.elementsOnPage;
     var dataToSend = {};
-    dataToSend["totalRecords"] = rawData["totalRecords"];
-    dataToSend["data"] = skip((req.query.page - 1) * req.query.elementsOnPage, req.query.page * req.query.elementsOnPage, rawData["data"]);
-    res.json(dataToSend);
+    var databaseOnGet = openDatabase();
+
+    databaseOnGet.serialize(function() {
+        databaseOnGet.all("SELECT COUNT(rowid) FROM address", function(err, row) {
+            dataToSend["totalRecords"] = row[0]["COUNT(rowid)"];
+            
+        });
+        databaseOnGet.all("SELECT * FROM address WHERE rowid > " + from + " AND rowid <= " + to, function(err, row) {
+            dataToSend["data"] = row;
+            databaseOnGet.close(); 
+            res.json(dataToSend);
+        });
+    });  
 });
 
-function updateSource(source) {
-    fs.writeFileSync(__dirname + "/data.json", JSON.stringify(source));
-}
-
 app.post('/post', function(req, res) {
-    var sourceFile = getData();
-    var newId = generateId(sourceFile["data"]);
-    var newData = {
-        _id: newId,
-        city: req.body.city,
-        street: req.body.street,
-        state: req.body.state
-    }
-    sourceFile["data"].push(newData);
-    sourceFile["totalRecords"]++;
-    updateSource(sourceFile);
-    res.end();
+    var newId; 
+    var currentIDs;
+
+    var databaseOnPost = openDatabase();   
+    databaseOnPost.all("SELECT _id FROM address", function(err, row) {
+        currentIDs = row;
+        newId = generateId(currentIDs);
+        databaseOnPost.run("INSERT INTO address (_id, state, city, street) VALUES (?, ?, ?, ?)", [newId, req.body.state, req.body.city, req.body.street], function(err) {
+            databaseOnPost.close(); 
+            res.end();
+        });
+
+    });    
 });
 
 app.delete('/delete/:id', function(req, res) {
-    var file = getData();
-    for(var j = 0; j < file["data"].length; j++) {
-        if(file["data"][j]["_id"] === req.params.id) {
-            file["data"].splice(j, 1);
-        }
-    }
-    file["totalRecords"]--;
-    updateSource(file);
-    res.end();
-})
+    var databaseOnDelete = openDatabase(); 
+    databaseOnDelete.run("DELETE FROM address WHERE _id = ?", [req.params.id], function(err) {
+        databaseOnDelete.close(); 
+        res.end();
+    });
+});
 
 app.use(express.static(__dirname + "/build"));
 
